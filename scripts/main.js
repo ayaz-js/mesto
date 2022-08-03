d6d54fce04a91dc4aa8452cea48cfdc78094bbe3
Serik Ayazbayev <jetheez@yandex.ru>
2022-08-03 14:55:18 +0600

    feat: доавить функционал редактирования профиля

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,28 @@ const editProfileButton = document.querySelector('.profile__edit-button');
 // closePopup buttons
 const closeProfileButton = document.querySelector('.form__close-button');
 const popupOverlay = document.querySelector('.popup__overlay');
+const formSaveButton = document.querySelector('.form__save-button');
+
+// profile
+const profileName = document.querySelector('.profile__name');
+const profileRole = document.querySelector('.profile__role');
+
+// inputs
+const inputName = document.querySelector('.form__input-name');
+const inputRole = document.querySelector('.form__input-role');
 
 // Functions
+function getProfile() {
+  inputName.value = profileName.textContent;
+  inputRole.value = profileRole.textContent;
+}
+
+function editProfile() {
+  profileName.textContent = inputName.value;
+  profileRole.textContent = inputRole.value;
+  closePopup()
+}
+
 function calcScrollWidth() {
   const scrollOverlay = document.createElement("div");
   scrollOverlay.style.width = "50px";
@@ -27,6 +47,7 @@ function openPopup() {
   popup.classList.add('popup_opened');
   document.body.style.overflow = 'hidden';
   document.body.style.borderRight = `${calcScrollWidth()}px solid black`;
+  getProfile()
   addEventListeners();
 }
 
@@ -39,7 +60,7 @@ function closePopup() {
 
 function closeOnEsc(event) {
   if (event.key === 'Escape' || event.key === 'Esc') {
-      closePopup(popup);
+      closePopup();
   }
 }
 
@@ -47,12 +68,14 @@ function addEventListeners() {
   closeProfileButton.addEventListener('click', closePopup);
   popupOverlay.addEventListener('click', closePopup);
   document.addEventListener('keyup', closeOnEsc);
+  formSaveButton.addEventListener('click', editProfile)
 }
 
 function removeEventListeners() {
-  closeProfileButton.addEventListener('click', closePopup);
-  popupOverlay.addEventListener('click', closePopup);
-  document.addEventListener('keyup', closeOnEsc);
+  closeProfileButton.removeEventListener('click', closePopup);
+  popupOverlay.removeEventListener('click', closePopup);
+  document.removeEventListener('keyup', closeOnEsc);
+  formSaveButton.removeEventListener('click', editProfile)
 }
 
 editProfileButton.addEventListener('click', openPopup);
