6f8a3e44f5516ffedf3378b0fac5af6fcb607077
Serik Ayazbayev <jetheez@yandex.ru>
2022-08-18 14:34:46 +0600

    feat: добавить функцию для рендора карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -20,6 +20,34 @@ const inputRole = formElement.querySelector('.form__input_type_role');
 // Elements
 const elements = document.querySelector('.elements');
 
+// card array
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // Functions
 function formSubmitHandler (event) {
   event.preventDefault();
@@ -72,6 +100,22 @@ function removeEventListeners() {
   formElement.removeEventListener('submit', formSubmitHandler);
   popup.removeEventListener('click', closeOnOverlay);
 }
+
+function renderCard(image, title) {
+  const cardTemplate = document.querySelector('.element__template').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.element__image');
+  const cardTitle = cardElement.querySelector('.element__title');
+
+  cardImage.src = image;
+  cardImage.alt = `Фотография: ${title}`
+  cardTitle.textContent = title;
+
+  elements.append(cardElement);
+}
+
+initialCards.forEach(item => renderCard(item.link, item.name));
 
 editProfileButton.addEventListener('click', openPopup);
 
