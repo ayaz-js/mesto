fcaeacf150d9860f64f1a26ed83b41d674e2b137
Serik Ayazbayev <jetheez@yandex.ru>
2022-10-12 11:45:04 +0600

    feat: добавить правила в webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,26 +20,37 @@ module.exports = {
     port: 8080
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: 'babel-loader',
-      exclude: '/node_modules/'
-    },
-    {
-      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-      type: 'asset/resource',
-    },
-    {
-      test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
         }
       },
-        'postcss-loader'
-      ]
-    },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+          'postcss-loader'
+        ]
+      },
     ]
   },
   plugins: [
